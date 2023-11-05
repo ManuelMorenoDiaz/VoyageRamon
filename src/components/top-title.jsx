3fe24e446f711ede8d5b3e4210e17aeb9c3831ce
ManuelMorenoDiaz <manupaps16@gmail.com>
2023-11-04 19:14:50 -0600

    Pantalla de Lugares

--- src/components/top-title.jsx
+++ src/components/top-title.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
 
-function TopTitle() {
+const TopTitle = (props) => {
+  const containerStyle = {
+    position: 'relative',
+    width: '100%',
+    height: '300px',
+    backgroundImage: `url("${props.imagen}")`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    color: 'white',
+    marginBottom:'20px'
+  };
+
+  const overlayStyle = {
+    content: '',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    background: 'rgba(0, 0, 0, 0.5)',
+  };
+
+  const textStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    textAlign: 'center',
+    fontSize: '24px', // Tamaño del texto
+  };
+
   return (
-    <div>top-title</div>
-  )
-}
+    <div style={containerStyle}>
+      <div style={overlayStyle}></div>
+      <div style={textStyle}>
+        <h1>{props.lugar}</h1>
+      </div>
+    </div>
+  );
+};
 
-export default TopTitle
+export default TopTitle;
