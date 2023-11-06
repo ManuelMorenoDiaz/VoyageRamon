4e8c80337cd0697a78b23457f755f23ac05e2184
Daniel Mtz J <martinez.daniel.isw@unipolidgo.edu.mx>
2023-11-06 10:26:46 -0600

    Vista de chat y modal agregado con css

---
+++ src/components/v_chat_modal_partners.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/components_styles/chat_modal.css';
+import { FaRegWindowClose, FaUser } from 'react-icons/fa';
+
+const V_chat_modal_partners = ({vc_closeModalPartners}) => {
+  return (
+    <div className="vc_modal_partners">
+      <div className="vc_modal_partners_container">
+        <div className="vc_modal_partners_container_title">
+          <h1 className='vc_modal_partners_title'>Compartir</h1>
+        </div>
+        <div className="vc_modal_partners_close">
+          <FaRegWindowClose onClick={vc_closeModalPartners}/>
+        </div>
+        <p className='vc_modal_partners_subtitle'>Añadir personas</p>
+        <input type="text" className='vc_modal_partners_input' />
+        <p className='vc_modal_partners_subtitle'>Compañeros de viaje</p>
+        <div className="vc_modal_partners_travels_container">
+          <div className="vc_modal_partners_travel_partners">
+            <FaUser className='vc_modal_partners_user_icon'/>
+            <div className='vc_modal_partners_user_information'>
+              <span className='vc_modal_partners_user_name'>Didier Urbina</span>
+              <span className='vc_modal_partners_user_email'>didier.urbina@gmail.com</span>
+            </div>
+          </div>
+          <div className="vc_modal_partners_travel_partners">
+            <FaUser className='vc_modal_partners_user_icon'/>
+            <div className='vc_modal_partners_user_information'>
+              <span className='vc_modal_partners_user_name'>Manuel Moreno</span>
+              <span className='vc_modal_partners_user_email'>manuel.moreno@gmail.com</span>
+            </div>
+          </div>
+          <div className="vc_modal_partners_travel_partners">
+            <FaUser className='vc_modal_partners_user_icon'/>
+            <div className='vc_modal_partners_user_information'>
+              <span className='vc_modal_partners_user_name'>Manuel Moreno</span>
+              <span className='vc_modal_partners_user_email'>manuel.moreno@gmail.com</span>
+            </div>
+          </div>
+          <div className="vc_modal_partners_travel_partners">
+            <FaUser className='vc_modal_partners_user_icon'/>
+            <div className='vc_modal_partners_user_information'>
+              <span className='vc_modal_partners_user_name'>Manuel Moreno</span>
+              <span className='vc_modal_partners_user_email'>manuel.moreno@gmail.com</span>
+            </div>
+          </div>
+        </div>
+        <button className='vc_modal_partners_button_accept'>Hecho</button> 
+      </div>
+    </div>
+  );
+};
+
+V_chat_modal_partners.propTypes = {
+  vc_closeModalPartners: PropTypes.func.isRequired,
+};
+
+
+export default V_chat_modal_partners;
--- src/views/user/chat.jsx
+++ src/views/user/chat.jsx
@@ -1,19 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from '../../components/nav-bar';
-import Footer from '../../components/footer'
+import '../../styles/chat.css';
+import V_chat_modal_partners from '../../components/v_chat_modal_partners';
+import { FaArrowLeft, FaPen, FaTrash } from 'react-icons/fa';
 
-function chat() {
+function Chat() {
+
+  const [vcModalPartners, setvcModalPartners] = useState(false);
+  const [vcModalRequests, setvcModalRequests] = useState(false);
+  
   return (
     <div>
       <Nav />
-      <div className="cont-chat">
-        <h2>Chat</h2>
-      </div> 
-      
-      <Footer/>
+      <div className="v_chat_containers">
+        <div className="v_chat_container_left">
+          <div className="v_chat_container_icons_top">
+            <FaArrowLeft/> 
+            <h1>Viaje a Cancun</h1>
+            <FaPen color='#13EF0A'/>
+            <FaTrash color='#E0342F'/>
+          </div>
+          <div className="v_chat_container_inputs_left">
+            <div className="v_chat_widers_inputs">
+              <label className='v_chat_label_dv'>Descripcion del viaje
+                <textarea className='v_chat_input_description' ></textarea>
+              </label>
+              <label className='v_chat_label_ld'>Lugar de destino
+                <input type="text" className='v_chat_input_place' />
+              </label>
+              <label className='v_chat_label_fv'>Fecha del viaje
+                <input type="date" className='v_chat_input_date' />
+              </label>
+            </div>
+            <div className="v_chat_dual_inputs">
+              <label className='v_chat_label_lp'>Limite de personas
+                <input type="text" name="" id="" className='v_chat_input_limit'/>
+              </label>
+              <label className='v_chat_label_pp'>Presupuesto propuesto
+                <input type="text" name="" id="" className='v_chat_input_budget' />
+              </label>
+              <button className='v_chat_button_partners' onClick={() => setvcModalPartners(true)}>Compañeros</button>
+              <button className='v_chat_button_requests'>Solicitudes</button>
+            </div>
+            <button className='v_chat_button_finish_travel'>Terminar viaje</button>
+          </div>
+        </div>
+        <div className="v_chat_container_rigth">
+          <div className="v_chat_container_inputs_rigth">
+            <div className="v_chat_message_received">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, rerum perferendis! Similique natus quam impedit, numquam nesciunt vitae sapiente repellendus reiciendis, laudantium ipsa ab, nisi officia voluptas deserunt voluptatem ipsam!
+            </div>
+            <div className="v_chat_message_received">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, rerum perferendis! Similique natus quam impedit, numquam nesciunt vitae sapiente repellendus reiciendis, laudantium ipsa ab, nisi officia voluptas deserunt voluptatem ipsam!
+            </div>
+            <div className="v_chat_message_received">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, rerum perferendis! Similique natus quam impedit, numquam nesciunt vitae sapiente repellendus reiciendis, laudantium ipsa ab, nisi officia voluptas deserunt voluptatem ipsam!
+            </div>
+            <div className="v_chat_message_sended">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente minus, perspiciatis enim sunt eos at architecto non nulla, reprehenderit labore ducimus ratione, tenetur expedita aspernatur repellendus perferendis aliquid pariatur magni!
+            </div>
+            <div className="v_chat_message_sended">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente minus, perspiciatis enim sunt eos at architecto non nulla, reprehenderit labore ducimus ratione, tenetur expedita aspernatur repellendus perferendis aliquid pariatur magni!
+            </div>
+          </div>
+          <input type="text" className='v_chat_input_send_message' placeholder='Enviar mensaje' />
+        </div>
+      </div>
+      {vcModalPartners && <V_chat_modal_partners vc_closeModalPartners={() => setvcModalPartners(false)} />}
     </div>
-   
   )
 }
 
-export default chat
+export default Chat;
