76c7c8fe98a934b7ece03a80fbdd69de164a4e3f
ManuelMorenoDiaz <manupaps16@gmail.com>
2023-11-29 10:56:59 -0600

    Ajuste de Carrusel de publicaciones

--- src/components/carrousel3D-places.jsx
+++ src/components/carrousel3D-places.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Swiper from 'swiper';
 import "swiper/swiper-bundle.css";
 import "../../src/styles/carrousel3D-places.css"; // Import the CSS file
@@ -6,30 +6,33 @@ import { Link } from "react-router-dom";
 
 function Carrousel3DPlaces(props) {
     const slides = props.placesByC;
+    const swiperContainerRef = useRef(null);
 
     useEffect(() => {
-        const swiper = new Swiper('.slider.swiper-container', {
-            slidesPerView: 1,
-            spaceBetween: 10,
-            centeredSlides: true,
-            loop: true,
-            grabCursor: true,
-            autoplay: {
-                delay: 5000,
-            },
-            effect: 'coverflow',
-            coverflowEffect: {
-                rotate: 50,
-                stretch: 0,
-                depth: 100,
-                modifier: 1,
-                slideShadows: true,
-            },
-        });
-    }, []);
+        if (swiperContainerRef.current && slides.length > 0) {
+            new Swiper(swiperContainerRef.current, {
+                slidesPerView: 1,
+                spaceBetween: 10,
+                centeredSlides: true,
+                loop: true,
+                grabCursor: true,
+                autoplay: {
+                    delay: 5000,
+                },
+                effect: 'coverflow',
+                coverflowEffect: {
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                },
+            });
+        }
+    }, [slides]);
 
     return (
-        <div className="slider swiper-container carrousel3D">
+        <div ref={swiperContainerRef} className="slider swiper-container carrousel3D">
             <div className="swiper-wrapper">
                 {slides.map((slide, index) => (
                     <div key={index} className="swiper-slide carrousel">
